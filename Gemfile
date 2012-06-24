682a9e4aa31dd89c8cb726bd737fa57719f05452
Jarek Plonski <jaroslaw.plonski@gmail.com>
2012-06-24 18:40:50 +0200

    Added user factories and tests for controller show

--- Gemfile
+++ Gemfile
@@ -14,5 +14,6 @@ group :test do
   gem 'rspec', '2.0.0.beta.18'
   gem 'spork', '0.9.0.rc8'
   gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails', '1.0'
 end
   
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Jarek Plonski"
+  user.email                  "jarek@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
